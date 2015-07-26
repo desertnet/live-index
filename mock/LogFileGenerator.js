ddc1aaaa67e9ff5c0ac845d3a55c58624ddf8231
Eric McCarthy <eric@limulus.net>
2015-07-25 19:00:52 -0700

    LogFileGenerator actually writes to a file

--- mock/LogFileGenerator.js
+++ mock/LogFileGenerator.js
@@ -2,10 +2,31 @@ import {EventEmitter} from "events"
 import fs from "fs"
 
 export default class LogFileGenerator extends EventEmitter {
+  constructor () {
+    super()
+
+    this._writer = null
+    this._numberOfLinesToWrite = 5
+  }
+
   writeLog (path) {
-    const writer = fs.createWriteStream(path)
-    writer.on("open", fd => {
+    this._writer = fs.createWriteStream(path)
+
+    this._writer.on("open", fd => {
       this.emit("created", path)
     })
+
+    setImmediate(() => this._writeUntilFlushed())
+  }
+
+  _writeUntilFlushed () {
+    if (this._numberOfLinesToWrite != 0) {
+      this._writer.write("foo:bar\n")
+      this._numberOfLinesToWrite -= 1
+      setImmediate(() => this._writeUntilFlushed())
+    }
+    else {
+      this.emit("flushed")
+    }
   }
 }
--- test/LogFileGeneratorTest.js
+++ test/LogFileGeneratorTest.js
@@ -16,11 +16,25 @@ describe("LogFileGenerator", () => {
   })
 
   describe("writeLog()", () => {
-    it("should create a file at the given path", done => {
-      const logPath = path.join(dir, "foo.log")
+    let logPath
 
+    beforeEach(() => {
+      logPath = path.join(dir, "foo.log")
+    })
+
+    it("should create a file at the given path", done => {
       generator.on("created", () => {
         assert(fs.existsSync(logPath))
+        return done()
+      })
+
+      generator.writeLog(logPath)
+    })
+
+    it("should write some entries to file", done => {
+      generator.on("flushed", () => {
+        const fileData = fs.readFileSync(logPath).toString()
+        assert(fileData.match(/:/))
         return done()
       })
 

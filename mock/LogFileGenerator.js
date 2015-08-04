8446d645dfd09533faf1725d5fd1017cdcd35634
Eric McCarthy <eric@limulus.net>
2015-08-04 11:59:13 -0700

    LogFileGenerator updates from tail-follow

--- mock/LogFileGenerator.js
+++ mock/LogFileGenerator.js
@@ -1,6 +1,7 @@
 import {EventEmitter} from "events"
 import fs from "fs"
 import crypto from "crypto"
+import assert from "assert"
 
 export default class LogFileGenerator extends EventEmitter {
   constructor () {
@@ -14,6 +15,7 @@ export default class LogFileGenerator extends EventEmitter {
 
   createLog (path) {
     this._writer = fs.createWriteStream(path)
+    this._currentPath = path
 
     this._writer.on("open", fd => {
       this._fd = fd
@@ -27,9 +29,7 @@ export default class LogFileGenerator extends EventEmitter {
 
   _writeUntilFlushed () {
     if (this._numberOfLinesToWrite != 0) {
-      let id = crypto.randomBytes(8).toString("base64").replace(/=/, "")
-      this._ids.push(id)
-      this._writer.write(`${id}:foo:bar\n`, () => {
+      this._writer.write(this.generateLogLine(), () => {
         fs.fsync(this._fd, () => {
           this._numberOfLinesToWrite -= 1
           setTimeout(() => this._writeUntilFlushed(), 1)
@@ -37,11 +37,42 @@ export default class LogFileGenerator extends EventEmitter {
       })
     }
     else {
-      this.emit("flushed")
+      setTimeout(() => this.emit("flushed"), 10)
     }
   }
 
   get ids () {
     return this._ids.slice()
+  }
+
+  get lines () {
+    return this.ids.map(id => LogFileGenerator.generateLogLineWithId(id))
+  }
+
+  get data () {
+    return this.lines.join("")
+  }
+
+  generateLogLine () {
+    const id = LogFileGenerator.generateId()
+    const line = LogFileGenerator.generateLogLineWithId(id)
+    this._ids.push(id)
+    return line
+  }
+
+  static generateLogLineWithId (id) {
+    return `${id}:foo:bar\n`
+  }
+
+  static generateId () {
+    return crypto.randomBytes(8).toString("base64").replace(/=/, "")
+  }
+
+  renameFile (newPath, cb) {
+    fs.rename(this._currentPath, newPath, err => {
+      assert.ifError(err)
+      this._currentPath = newPath
+      if (cb) return cb()
+    })
   }
 }
